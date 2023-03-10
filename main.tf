2d5bb4f8f8c96324cd39f5e76ffafe3a27e94600
prayas2409 <prayas.hemraj.pagade@gmail.com>
2023-03-10 13:12:19 +0530

    updated main

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ resource "aws_instance" "ec2_public" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
   key_name                    = var.key_name
-  security_groups             = ["${aws_security_group.ssh-security-group.id}"]
-  subnet_id                   = aws_subnet.public-subnet-1.id
+  security_groups             = [var.security_group]
+  subnet_id                   = var.public_subnet-1
   associate_public_ip_address = true
   #user_data                   = "${data.template_file.provision.rendered}"
   #iam_instance_profile = "${aws_iam_instance_profile.some_profile.id}"
